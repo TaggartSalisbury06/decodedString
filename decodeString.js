a9fe78c194a912ebd04bdec6e4ca507036fe5a2e
Lindsay Salisbury <lindsaysalisbury@Lindsays-Air.localdomain>
2023-03-06 10:48:09 -0700

    update challenge

--- decodeString.js
+++ decodeString.js
@@ -2,15 +2,13 @@
 
 function decodeStr(str) {
   let shift = parseInt(str)
-
-    if (isNaN(shift)){
-      throw new Error('Invalid Input')
-    }
-
     let result = ''
     for (let i = 1; i < str.length; i++) {
       let charCode = str.charCodeAt(i)
-      if (charCode >= 97 && charCode <= 122){
+      if (charCode >= 65 && charCode <= 90){
+        let shiftedCode = ((charCode - 65 + shift) % 26) + 65
+        result = String.fromCharCode(shiftedCode)
+      } else if (charCode >= 97 && charCode <= 122){
         let shiftedCode = ((charCode - 97 + shift) % 26) + 97
         result = String.fromCharCode(shiftedCode)
       } else {
@@ -19,4 +17,4 @@ function decodeStr(str) {
     return result
   }
 }
-console.log(decodeStr('1a'))
+console.log(decodeStr('1A'))
--- example.js
+++ example.js
@@ -1,21 +1,23 @@
-function shiftString(str) {
-  // Parse the number at the beginning of the string
-  const shift = parseInt(str);
-  if (isNaN(shift)) {
-    throw new Error("Invalid input");
-  }
+// function shiftString(str) {
+//   // Parse the number at the beginning of the string
+//   const shift = parseInt(str);
+//   if (isNaN(shift)) {
+//     throw new Error("Invalid input");
+//   }
   
-  let result = "";
-  for (let i = 1; i < str.length; i++) {
-    const charCode = str.charCodeAt(i);
-    if (charCode >= 97 && charCode <= 122) {
-      // Shift lowercase letters by the given amount
-      const shiftedCode = ((charCode - 97 + shift) % 26) + 97;
-      result += String.fromCharCode(shiftedCode);
-    } else {
-      throw new Error("Invalid input");
-    }
-  }
+//   let result = "";
+//   for (let i = 1; i < str.length; i++) {
+//     const charCode = str.charCodeAt(i);
+//     if (charCode >= 97 && charCode <= 122) {
+//       // Shift lowercase letters by the given amount
+//       const shiftedCode = ((charCode - 97 + shift) % 26) + 97;
+//       result += String.fromCharCode(shiftedCode);
+//     } else {
+//       throw new Error("Invalid input");
+//     }
+//   }
   
-  return result;
-}
+//   return result;
+// }
+let ltr = 'A'
+console.log(((ltr.charCodeAt() - 65) % 26) + 65)
